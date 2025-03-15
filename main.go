7eed0f93d193c6137efc60b1211815b7375e1877
Adik Soleh <adiksoleh4@gmail.com>
2025-03-15 20:32:16 +0700

    fixing error in main.go

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"golang-rest-api/config"
+	"golang-rest-api/migrations"
+	"golang-rest-api/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-
 	app := fiber.New()
 
-	app.Get("/developers", developers)
+	// Koneksi database
+	config.ConnectDatabase()
 
+	// Jalankan migrasi
+	migrations.RunMigration()
+
+	// Setup routes
+	routes.SetupRoutes(app)
+
+	// Jalankan server
 	app.Listen(":3000")
-
-}
-
-func developers(ctx *fiber.Ctx) error {
-	return ctx.Status(200).JSON("data")
 }
