246cf8b7121964178bf89b4831f2d1b788e516e0
Adik Soleh <adiksoleh4@gmail.com>
2025-03-15 23:26:06 +0700

    refactor: add relation db profile

--- models/user.go
+++ models/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"-"`
+	Name     string   `json:"name"`
+	Email    string   `json:"email" gorm:"unique"`
+	Password string   `json:"-"`
+	Profile  *Profile `json:"profile" gorm:"constraint:OnDelete:CASCADE;"`
 }
